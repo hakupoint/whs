266b4ea0d4a48804635644c7d4d6715429b93f07
hakupoint <hakupoint15@gmail.com>
2018-03-07 17:42:48 +0800

    添加过滤文件,添加config文件

--- main.go
+++ main.go
@@ -1,15 +1,62 @@
 package main
 
 import (
+	"io/ioutil"
+	"os/user"
+	"path/filepath"
+	"fmt"
 	"os"
 	"github.com/urfave/cli"
+	"github.com/BurntSushi/toml"
+)
+const (
+	version = "0.1"
+	name = "whs"
+	serverPort = 10290
 )
 type cmd string
 
+var conf = &Config{}
 type Config struct {
 	Path   string
-	OutDir cmd
+	OutDir string
 	Edit   cmd
+	ServerPort int
+	TemplateFile string
+}
+
+func (c *Config) read(p string){
+	f, _ := os.Open(p)
+	b, _ := ioutil.ReadAll(f)
+	if _, err := toml.Decode(string(b), &conf); err == nil {
+		fmt.Printf("%+v", conf)
+	}
+}
+
+func (c *Config) init() {
+	usr, err := user.Current()
+	if err != nil {
+		fmt.Println(err)
+	}
+	p := filepath.Join(usr.HomeDir, "whs.toml")
+	if _, err := os.Stat(p); os.IsNotExist(err) {
+		c.createConfigFile(p)
+	}
+	c.read(p)
+}
+
+func (c *Config) createConfigFile(p string){
+	f, _ := os.Create(p)
+	c.Path = p
+	c.OutDir = filepath.Join(p, "_post")
+	c.ServerPort = serverPort
+	c.Edit = "vim"
+	e := toml.NewEncoder(f)
+	err := e.Encode(c)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
 }
 
 var command = []cli.Command{
@@ -30,6 +77,14 @@ var command = []cli.Command{
 		},
 	},
 	{
+		Name: "remove",
+		Aliases: []string{"r"},
+		Usage:   "remove note file",
+		Action:  func(c *cli.Context) error {
+			return nil
+		},
+	},
+	{
 		Name: "todo",
 		Aliases: []string{"t"},
 		Usage:   "todo list",
@@ -45,6 +100,14 @@ var command = []cli.Command{
 			return nil
 		},
 	},
+	{
+		Name: "config",
+		Aliases: []string{"c"},
+		Usage:   "edit config file.",
+		Action:  func(c *cli.Context) error {
+			return nil
+		},
+	},
 }
 
 func add(c *cli.Context) error {
@@ -53,8 +116,12 @@ func add(c *cli.Context) error {
 func list(c *cli.Context) error {
 	return nil
 }
+func remove(c *cli.Context) error {
+	return nil
+}
 func todo(c *cli.Context) error {
 	return nil
+
 }
 func grep(c *cli.Context) error {
 	return nil
@@ -62,5 +129,9 @@ func grep(c *cli.Context) error {
 
 func main() {
 	app := cli.NewApp()
+	app.Commands = command
+	app.Version = version
+	app.Name = name
 	app.Run(os.Args)
+	conf.init()
 }
